4d44972fbd393e55bbd90c56a8d15e89c5787522
Theo Beers <theo.beers@fu-berlin.de>
2021-04-07 10:22:56 +0200

    Add etymology mode

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,39 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
 use std::process::Command;
-use std::{env, fs, str};
+use std::{fs, str};
 
+use clap::{crate_version, App, Arg};
 use isahc::prelude::*;
 use regex::Regex;
 use scraper::{ElementRef, Html, Selector};
 use tempfile::NamedTempFile;
 
 fn main() -> Result<(), isahc::Error> {
-    // Collect args
-    let args: Vec<String> = env::args().collect();
+    // Set up CLI
+    let matches = App::new("ahd-scrape")
+        .version(crate_version!())
+        .author("Theo Beers <theo.beers@fu-berlin.de>")
+        .about("A simple CLI dictionary")
+        .arg(
+            Arg::with_name("etym")
+                .short("e")
+                .long("etymology")
+                .help("Search for etymology instead of definition"),
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("The word or phrase to look up")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
 
-    // If a word wasn't given, panic
-    if args.len() < 2 {
-        panic!("No word was provided");
-    };
+    // Do we have the etymology flag?
+    let etym_mode: bool = matches.is_present("etym");
 
-    // First real arg is the desired word
-    // Lowercase it for good measure (performance impact seems negligible)
-    let desired_word = &args[1].to_lowercase();
+    // Take input and lowercase it
+    let desired_word = matches.value_of("INPUT").unwrap().to_lowercase();
 
     // See if we can get the home directory
     let home_dir = home::home_dir();
@@ -32,7 +46,7 @@ fn main() -> Result<(), isahc::Error> {
     // Set up a string for a possible path
     let mut notional_file_path = String::new();
 
-    // We will also need to check for the cache directory
+    // We also need to check for the cache directory
     let mut cache_path = String::new();
 
     // Let's make a global variable for cache directory availability
@@ -46,15 +60,27 @@ fn main() -> Result<(), isahc::Error> {
         // println!("Home dir path: {}", home_dir_str);
 
         notional_file_path.push_str(&home_dir_str);
-        notional_file_path.push_str("/.ahd-scrape/cache/");
-        notional_file_path.push_str(desired_word);
-        notional_file_path.push_str(".txt");
+
+        if etym_mode {
+            notional_file_path.push_str("/.ahd-scrape/etym-cache/");
+            notional_file_path.push_str(&desired_word);
+            notional_file_path.push_str(".txt");
+        } else {
+            notional_file_path.push_str("/.ahd-scrape/def-cache/");
+            notional_file_path.push_str(&desired_word);
+            notional_file_path.push_str(".txt");
+        }
 
         // println!("Desired file path: {}", notional_file_path);
 
         // Also put together the cache directory path
         cache_path.push_str(&home_dir_str);
-        cache_path.push_str("/.ahd-scrape/cache");
+
+        if etym_mode {
+            cache_path.push_str("/.ahd-scrape/etym-cache");
+        } else {
+            cache_path.push_str("/.ahd-scrape/def-cache");
+        }
 
         // println!("Cache dir path: {}", cache_path);
 
@@ -71,7 +97,7 @@ fn main() -> Result<(), isahc::Error> {
         // If it isn't there, try creating it
         if !cache_path_exists {
             let try_create_cache = fs::create_dir_all(&cache_path);
-            if let Ok(_) = try_create_cache {
+            if try_create_cache.is_ok() {
                 cache_path_exists = true;
             }
         }
@@ -104,7 +130,7 @@ fn main() -> Result<(), isahc::Error> {
             let mut contents = String::new();
             let try_read = file.read_to_string(&mut contents);
 
-            if let Ok(_) = try_read {
+            if try_read.is_ok() {
                 // Success? Print file contents (to be piped) and return
                 print!("{}", contents);
 
@@ -117,45 +143,64 @@ fn main() -> Result<(), isahc::Error> {
     // This is, of course, the default case
 
     // Assemble URL
-    let mut dictionary_url = "https://www.thefreedictionary.com/".to_string();
-    dictionary_url += desired_word;
+
+    let mut lookup_url: String;
+
+    if etym_mode {
+        lookup_url = "https://www.etymonline.com/word/".to_string();
+        lookup_url += &desired_word.replace(" ", "%20");
+    } else {
+        lookup_url = "https://www.thefreedictionary.com/".to_string();
+        lookup_url += &desired_word.replace(" ", "+");
+    }
 
     // Make the request
-    let mut response = isahc::get(dictionary_url)?;
+    let mut response = isahc::get(lookup_url)?;
 
     // Get the document text
     let response_text = response.text()?;
 
     // Set up a regex to split the document
+    // This won't do anything in etymology mode
     let re_thesaurus = Regex::new(r#"<div id="Thesaurus">"#).unwrap();
 
-    // Split document into two chunks
+    // Split document into two chunks (if in definition mode)
     // Otherwise we could blow a bunch of time parsing the whole thing
     let chunks: Vec<&str> = re_thesaurus.split(&response_text).collect();
 
     // Parse the first chunk, which is the one we want
+    // For an etymology entry, the "first chunk" is the whole document
     let parsed_chunk = Html::parse_fragment(chunks[0]);
 
     // Set up a selector for the relevant section
-    let section_selector = Selector::parse(r#"div#Definition section[data-src="hm"]"#).unwrap();
+    let section_selector = match etym_mode {
+        true => Selector::parse(r#"div[class^="word--"]"#).unwrap(),
+        _ => Selector::parse(r#"div#Definition section[data-src="hm"]"#).unwrap(),
+    };
 
     // Run the select iterator and collect the result in a vec
-    // This should yield either one item, or nothing
+    // For definition lookup, this should yield either one item, or nothing
+    // For etymology lookup, it could yield multiple sections
     let section_vec: Vec<ElementRef> = parsed_chunk.select(&section_selector).collect();
 
-    // Set up a few more selectors, just for the desired elements
+    // Set up a few more selectors for desired elements (in definition mode)
     let element_selectors = Selector::parse("div.pseg, h2, hr.hmsep").unwrap();
 
     // Check to see if we got the section we wanted
-    if section_vec.len() > 0 {
-        // If so, we'll iterate over the section and grab elements
-        // This maintains document order, unlike pup, which drove me nuts
-
+    if !section_vec.is_empty() {
         // Set up a string to hold results
         let mut results = String::new();
 
-        for element in section_vec[0].select(&element_selectors) {
-            results.push_str(&element.html());
+        if etym_mode {
+            // If etymology, just push everything from any sections
+            for item in section_vec.iter() {
+                results.push_str(&item.html());
+            }
+        } else {
+            // If definition, push selected elements from first/only section
+            for element in section_vec[0].select(&element_selectors) {
+                results.push_str(&element.html());
+            }
         }
 
         // Update: now we're calling out to Pandoc from the Rust program
@@ -163,48 +208,88 @@ fn main() -> Result<(), isahc::Error> {
         // I'm using tempfiles to feed input to Pandoc
         // At the end, we have a nice plain text file to cache
 
-        let mut pandoc_input = NamedTempFile::new().expect("Failed to create tempfile");
-        write!(pandoc_input, "{}", results).expect("Failed to write to tempfile");
+        let mut final_output = String::new();
 
-        let pandoc_1 = Command::new("pandoc")
-            .arg(pandoc_input.path())
-            .arg("-f")
-            .arg("html+smart-native_divs")
-            .arg("-t")
-            .arg("markdown")
-            .arg("--wrap=none")
-            .output()
-            .expect("Failed to execute Pandoc");
+        if etym_mode {
+            let mut pandoc_input = NamedTempFile::new().expect("Failed to create tempfile");
+            write!(pandoc_input, "{}", results).expect("Failed to write to tempfile");
 
-        let output_1 =
-            str::from_utf8(&pandoc_1.stdout).expect("Failed to convert Pandoc output to string");
+            let pandoc_1 = Command::new("pandoc")
+                .arg(pandoc_input.path())
+                .arg("-f")
+                .arg("html+smart-native_divs")
+                .arg("-t")
+                .arg("markdown")
+                .arg("--wrap=none")
+                .output()
+                .expect("Failed to execute Pandoc");
 
-        let re_list_1 = regex::Regex::new(r"\n\*\*(?P<a>\d+\.)\*\*").unwrap();
-        let after_1 = re_list_1.replace_all(&output_1, "\n$a").to_string();
+            let output_1 = str::from_utf8(&pandoc_1.stdout)
+                .expect("Failed to convert Pandoc output to string");
 
-        let re_list_2 = regex::Regex::new(r"\n\*\*(?P<b>[a-z]\.)\*\*").unwrap();
-        let after_2 = re_list_2.replace_all(&after_1, "\n    $b").to_string();
+            let re_quotes = regex::Regex::new(r#"\\""#).unwrap();
+            let after = re_quotes.replace_all(&output_1, r#"""#).to_string();
 
-        let mut input_file_2 = NamedTempFile::new().expect("Failed to create tempfile");
-        write!(input_file_2, "{}", after_2).expect("Failed to write to tempfile");
+            let mut input_file_2 = NamedTempFile::new().expect("Failed to create tempfile");
+            write!(input_file_2, "{}", after).expect("Failed to write to tempfile");
 
-        let pandoc_2 = Command::new("pandoc")
-            .arg(input_file_2.path())
-            .arg("-t")
-            .arg("plain")
-            .output()
-            .expect("Failed to execute Pandoc");
+            let pandoc_2 = Command::new("pandoc")
+                .arg(input_file_2.path())
+                .arg("-t")
+                .arg("plain")
+                .output()
+                .expect("Failed to execute Pandoc");
 
-        let final_output =
-            str::from_utf8(&pandoc_2.stdout).expect("Failed to convert Pandoc output to string");
+            let output_2 = str::from_utf8(&pandoc_2.stdout)
+                .expect("Failed to convert Pandoc output to string");
+
+            final_output.push_str(output_2);
+        } else {
+            let mut pandoc_input = NamedTempFile::new().expect("Failed to create tempfile");
+            write!(pandoc_input, "{}", results).expect("Failed to write to tempfile");
+
+            let pandoc_1 = Command::new("pandoc")
+                .arg(pandoc_input.path())
+                .arg("-f")
+                .arg("html+smart-native_divs")
+                .arg("-t")
+                .arg("markdown")
+                .arg("--wrap=none")
+                .output()
+                .expect("Failed to execute Pandoc");
+
+            let output_1 = str::from_utf8(&pandoc_1.stdout)
+                .expect("Failed to convert Pandoc output to string");
+
+            let re_list_1 = regex::Regex::new(r"\n\*\*(?P<a>\d+\.)\*\*").unwrap();
+            let after_1 = re_list_1.replace_all(&output_1, "\n$a").to_string();
+
+            let re_list_2 = regex::Regex::new(r"\n\*\*(?P<b>[a-z]\.)\*\*").unwrap();
+            let after_2 = re_list_2.replace_all(&after_1, "\n    $b").to_string();
+
+            let mut input_file_2 = NamedTempFile::new().expect("Failed to create tempfile");
+            write!(input_file_2, "{}", after_2).expect("Failed to write to tempfile");
+
+            let pandoc_2 = Command::new("pandoc")
+                .arg(input_file_2.path())
+                .arg("-t")
+                .arg("plain")
+                .output()
+                .expect("Failed to execute Pandoc");
+
+            let output_2 = str::from_utf8(&pandoc_2.stdout)
+                .expect("Failed to convert Pandoc output to string");
+
+            final_output.push_str(output_2);
+        }
 
         // If the cache path proved available earlier, try to create a file
         if cache_path_exists {
-            let new_file = File::create(&notional_file_path);
+            let try_file = File::create(&notional_file_path);
 
             // If we have the new file, write the results into it
             // Again, errors are just ignored
-            if let Ok(mut file) = new_file {
+            if let Ok(mut file) = try_file {
                 let _ = file.write_all(final_output.as_bytes());
             };
         }
@@ -212,12 +297,17 @@ fn main() -> Result<(), isahc::Error> {
         // We still need to print results, of course
         print!("{}", final_output);
     } else {
-        // Otherwise let's check for a list of similar words
+        // If we didn't get an etymology result, stop here
+        if etym_mode {
+            panic!("Etymology not found");
+        }
+
+        // In dictionary mode, we can check for a list of similar words
         let suggestions_selector = Selector::parse("ul.suggestions li").unwrap();
         let suggestions_vec: Vec<ElementRef> = parsed_chunk.select(&suggestions_selector).collect();
 
         // Again, see if we got anything
-        if suggestions_vec.len() > 0 {
+        if !suggestions_vec.is_empty() {
             // If so, collect results
             let mut results = String::new();
 
@@ -241,9 +331,8 @@ fn main() -> Result<(), isahc::Error> {
             let output_1 = str::from_utf8(&pandoc_1.stdout)
                 .expect("Failed to convert Pandoc output to string");
 
-            // Print an explanatory message first
+            // Print an explanatory message, then the results
             print!("Did you mean:\n\n");
-
             print!("{}", output_1);
         } else {
             // If still no dice, panic
